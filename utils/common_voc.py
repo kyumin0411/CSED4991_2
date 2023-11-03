e528ad2e461777738a6742cea993aada5fac2de6
rbalsgood <rbalsgood@postech.ac.kr>
2023-11-03 14:03:49 +0900

    edit path of checkpoint

--- utils/common_voc.py
+++ utils/common_voc.py
@@ -357,7 +357,7 @@ def get_parser(mode='sup_active'):
 
 
     r" logging"
-    parser.add_argument('-p', '--model_save_dir', default='./checkpoint/default')
+    parser.add_argument('-p', '--model_save_dir', default='/home/kyumin/checkpoint/default')
     parser.add_argument('--save_feat_dir', type=str, default='log/default', help='Region feature directory.')
     parser.add_argument('--skip_first_eval', action='store_true', default=False)
     parser.add_argument('--wandb_tags', nargs='+', default=None)
