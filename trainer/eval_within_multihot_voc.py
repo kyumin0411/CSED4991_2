7664fd6398d8e10cc50efe8d4fe761bd56676a9c
rbalsgood <rbalsgood@postech.ac.kr>
2023-11-07 22:00:00 +0900

    add pdb for debugging

--- trainer/eval_within_multihot_voc.py
+++ trainer/eval_within_multihot_voc.py
@@ -51,7 +51,7 @@ class ActiveTrainer(BaseTrainer):
         iou_helper = MeanIoU(self.num_classes + 1, self.args.ignore_idx)
         iou_helper._before_epoch()
         N = loader.__len__()
-
+        import pdb; pdb.set_trace()
         ### model forward
         self.net.eval()
         with torch.no_grad():
